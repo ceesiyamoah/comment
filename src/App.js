6aec68110c59d090fcc53f7703308c78a064d00c
Cyril <ccyamoah@gmail.com>
2021-12-28 03:03:06 +0000

    comment styling done

--- src/App.js
+++ src/App.js
@@ -1,68 +1,52 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
 import './App.scss';
-import avatar from './images/avatars/image-amyrobson.png';
+import Comment from './components/Comment';
+import data from './data.json';
+
 function App() {
+	const [currentUser, setCurrentUser] = useState(data.currentUser);
+	const [comments, setComments] = useState(data.comments);
+
 	return (
-		<>
-			<section className='comment'>
-				<div className='comment__counter'>
-					<div className='counter'>
-						<button className='counter__button'>
-							<svg width='11' height='11' xmlns='http://www.w3.org/2000/svg'>
-								<path
-									className='counter__button-icon'
-									d='M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z'
-									fill='#C5C6EF'
+		<div style={{ width: '60%', margin: 'auto' }}>
+			{comments.map((comment) => (
+				<React.Fragment key={comment.id}>
+					<Comment
+						content={comment.content}
+						duration={comment.createdAt}
+						count={comment.score}
+						name={comment.user.username}
+						avatar={comment.user.image.png}
+						currentUser={comment.user.username === currentUser.username}
+					/>
+					{comment.replies && (
+						<section className='comment-after'>
+							{comment.replies.map((reply) => (
+								<Comment
+									key={reply.id}
+									content={reply.content}
+									duration={reply.createdAt}
+									count={reply.score}
+									name={reply.user.username}
+									avatar={reply.user.image.png}
+									currentUser={reply.user.username === currentUser.username}
 								/>
-							</svg>{' '}
-						</button>
-						<span className='counter__number'>5</span>
-						<button className='counter__button'>
-							<svg width='11' height='3' xmlns='http://www.w3.org/2000/svg'>
-								<path
-									className='counter__button-icon'
-									d='M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z'
-									fill='#C5C6EF'
-								/>
-							</svg>
-						</button>
-					</div>
-				</div>
-				<div className='comment__content'>
-					<div className='comment__content-header'>
-						<header className='left'>
-							<img src={avatar} alt='' className='user' />
-							<span className='user__name'>Amy rep</span>
-							<span className='duration'>1 month</span>
-						</header>
-						<header className='right'>
-							<button className='counter__button counter__button-text'>
-								<svg width='14' height='13' xmlns='http://www.w3.org/2000/svg'>
-									<path
-										className='counter__button-icon'
-										d='M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z'
-										fill='#5357B6'
-									/>
-								</svg>
-								<span>Reply</span>
-							</button>
-						</header>
-					</div>
-					<div className='comment__content-body'>
-						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem
-						modi nisi tempore ab maxime nam sunt ut sequi quia eveniet, iusto,
-						blanditiis quibusdam porro iste repellendus eligendi aliquam? Esse
-						architecto in at consectetur rerum aspernatur aut fugiat quos sint
-						eligendi, et officiis? Harum enim quas, similique cum commodi sit
-						consequatur! Lorem ipsum dolor sit amet consectetur adipisicing
-						elit. Quas, officiis consequuntur? Repellat, ut debitis. Adipisci
-						enim cumque recusandae incidunt asperiores, veniam tempore commodi,
-						et consequatur ad soluta mollitia dignissimos odit sed quod harum
-						voluptatem fugit voluptas ut repellendus iusto voluptates fuga
-						dolores cum. Optio nulla vel laboriosam eius aspernatur deleniti!
-					</div>
-				</div>
+							))}
+						</section>
+					)}
+				</React.Fragment>
+			))}
+			<section className='comment flex'>
+				<img
+					src={process.env.PUBLIC_URL + currentUser.image.png}
+					alt='current user'
+					className='user'
+				/>
+				<textarea className='text' placeholder='Add comment...'></textarea>
+				<button className='counter__button-text main_button'>SEND</button>
 			</section>
-		</>
+		</div>
 	);
 }
 
